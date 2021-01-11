cf6f9f65b34d1b00a103d09cd8e2b1e1822e4b3a
xingzheng <xingzheng@outlook.com>
2021-01-11 22:31:54 +0800

    Modify structure

---
+++ CodeFirstSample/EfCodeFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
