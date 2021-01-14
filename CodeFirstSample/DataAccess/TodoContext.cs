5555e13357e6d8d13256115082aac8ef388955da
xingzheng <xingzheng@outlook.com>
2021-01-14 12:26:21 +0800

    add DbContext

---
+++ CodeFirstSample/DataAccess/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
