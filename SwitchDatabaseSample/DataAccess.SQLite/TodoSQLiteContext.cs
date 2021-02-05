e2333b2f50ea9c58866a69da1bf0a7bc66969b41
xingzheng <xingzheng@outlook.com>
2021-02-05 22:12:04 +0800

    adapt to SQLite

---
+++ SwitchDatabaseSample/DataAccess.SQLite/TodoSQLiteContext.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess.SQLite
+{
+    public class TodoSQLiteContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=SwitchDB.db");
+    }
+}
